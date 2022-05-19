eaabf82d7e6a502921de2472f60b9bdafa0a9226
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2022-05-19 15:34:09 +0400

    add CMakeLists and conanfile

--- cpp/LinksPSQL.h
+++ cpp/LinksPSQL.h
@@ -1,128 +1,128 @@
+#include <array>
+#include <vector>
+
 #include <pqxx/pqxx>
+#include <Platform.Data.h>
 
-namespace Platform::Data::Doublets
+template<typename TLink>
+struct LinksPSQL
 {
-    template<typename TLink>
-    struct LinksPSQL
-    {
-        using LinkType = LinksOptions<>::LinkType;
+    using LinkType = Platform::Data::LinksOptions<>::LinkType;
 
-        explicit LinksPSQL(const std::string dbopts) : opts(dbopts)
+    explicit LinksPSQL(const std::string dbopts) : opts(dbopts)
+    {
+        query = "CREATE TABLE IF NOT EXISTS Links(id bigint, from_id bigint, to_id bigint);";
+        line.retrieve(line.insert(query));
+        GetIndex();
+    }
+
+    ~LinksPSQL()
+    {
+        line.complete();
+        transaction.commit();
+        connection.close();
+    }
+
+    bool Exists(auto&& Index)
+    {
+        auto result = line.retrieve(line.insert("SELECT * FROM Links WHERE id = " + std::to_string(Index) + ";"));
+        return result[0][0].c_str() != "";
+    }
+
+    void Create(const LinkType& substitution)
+    {
+        line.insert("INSERT INTO Links VALUES (" + std::to_string(++index) + ", "
+        + std::to_string(substitution[0]) + ", " + std::to_string(substitution[1]) + ");");
+    }
+
+    void Update(const LinkType& restrictions, const LinkType& substitution)
+    {
+        if (std::size(restrictions) == 1 || std::size(restrictions) == 3)
         {
-            query = "CREATE TABLE IF NOT EXISTS Links(id bigint, from_id bigint, to_id bigint);";
-            line.retrieve(line.insert(query));
-            GetIndex();
+            query = "UPDATE Links SET from_id = " + std::to_string(substitution[0]) + ", to_id = "
+                    + std::to_string(substitution[1]) + " WHERE id = " + std::to_string(restrictions[0]) + ";";
         }
-        
-        ~LinksPSQL()
+        if (std::size(restrictions) == 2)
         {
-            line.complete();
-            transaction.commit();
-            connection.close();
+            query = "UPDATE Links SET from_id = " + std::to_string(substitution[0]) + ", to_id = "
+                    + std::to_string(substitution[1]) + " WHERE from_id = " + std::to_string(restrictions[0])
+                    + "AND to_id = " + std::to_string(restrictions[1]) + ";";
         }
-        
-        bool Exists(auto&& Index)
+        line.insert(query);
+    }
+
+    void Delete(const LinkType& restrictions)
+    {
+        if (!this->Exists(restrictions[0]))
+            std::cout << "You can`t delete non-existent link.";
+        else
         {
-            auto result = line.retrieve(line.insert("SELECT * FROM Links WHERE id = " + std::to_string(Index) + ";"));
-            return result[0][0].c_str() != "";
-        }
-        
-        void Create(const LinkType& substitution)
-        {
-            line.insert("INSERT INTO Links VALUES (" + std::to_string(++_index) + ", "
-            + std::to_string(substitution[0]) + ", " + std::to_string(substitution[1]) + ");");
-        }
-        
-        void Update(const LinkType& restrictions, const LinkType& substitution)
-        {
-            if (std::size(restrictions) == 1 || std::size(restrictions) == 3)
-            {
-                query = "UPDATE Links SET from_id = " + std::to_string(substitution[0]) + ", to_id = "
-                + std::to_string(substitution[1]) + " WHERE id = " + std::to_string(restrictions[0]) + ";";  
-            }
-            if (std::size(restrictions) == 2)
-            {
-                query = "UPDATE Links SET from_id = " + std::to_string(substitution[0]) + ", to_id = "
-                + std::to_string(substitution[1]) + " WHERE from_id = " + std::to_string(restrictions[0])
-                + "AND to_id = " + std::to_string(restrictions[1]) + ";";
-            }
+            query = "DELETE FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
             line.insert(query);
         }
-        
-        void Delete(const LinkType& restrictions)
+    }
+
+    void Complete() { line.complete(); }
+
+    std::uint64_t Count(const LinkType& restrictions)
+    {
+        using namespace Platform::Data;
+        LinksConstants<TLink> constants;
+        if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
+            query = "SELECT COUNT(*) FROM Links;";
+        else if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
+            query = "SELECT COUNT(*) FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
+        else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
+            query = "SELECT COUNT(*) FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + ";";
+        else if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
+            query = "SELECT COUNT(*) FROM Links WHERE to_id = " + std::to_string(restrictions[2]) + ";";
+        else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
+            query = "SELECT COUNT(*) FROM Links WHERE from_id = " + std::to_string(restrictions[1])
+                    + "AND to_id = " + std::to_string(restrictions[2]) + ";";
+        auto result = line.retrieve(line.insert(query));
+        return result[0][0].as<std::uint64_t>();
+    }
+
+    std::vector<std::array<TLink, 3>> Each(const LinkType& restrictions)
+    {
+        using namespace Platform::Data;
+        LinksConstants<TLink> constants;
+        if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
+            query = "SELECT * FROM Links;";
+        else if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
+            query = "SELECT * FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
+        else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
+            query = "SELECT * FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + ";";
+        else if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
+            query = "SELECT * FROM Links WHERE to_id = " + std::to_string(restrictions[2]) + ";";
+        else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
+            query = "SELECT * FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + "AND to_id = "
+                    + std::to_string(restrictions[2]) + ";";
+        auto result = line.retrieve(line.insert(query));
+        std::vector<std::array<TLink, 3>> links {};
+        std::array<TLink, 3> link {};
+        for(int i{}; i<result.size(); i++)
         {
-            if (!this->Exists(restrictions[0]))
-                std::cout << "You can`t delete non-existent link.";
-            else
+            for(int j{}; j<3; j++)
             {
-                query = "DELETE FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
-                line.insert(query);
+                link[j] = result[i][j].as<TLink>();
+                links.push_back(link);
             }
         }
+        return links;
+    }
 
-        void Complete()
-        {
-            line.complete();
-        }
-        
-        std::uint64_t Count(const LinkType& restrictions)
-        {
-            LinksConstants<TLink> constants;
-            if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
-                query = "SELECT COUNT(*) FROM Links;";
-            else if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
-                query = "SELECT COUNT(*) FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
-            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
-                query = "SELECT COUNT(*) FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + ";";
-            else if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
-                query = "SELECT COUNT(*) FROM Links WHERE to_id = " + std::to_string(restrictions[2]) + ";";
-            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
-                query = "SELECT COUNT(*) FROM Links WHERE from_id = " + std::to_string(restrictions[1]) +
-                        "AND to_id = " + std::to_string(restrictions[2]) + ";";
-            auto result = line.retrieve(line.insert(query));
-            return result[0][0].as<std::uint64_t>();
-        }
+    private: std::string opts{};
+    private: std::string query{};
+    private: std::uint64_t index{};
 
-        std::vector<Link<TLink>> Each(const LinkType& restrictions)
-        {
-            LinksConstants<TLink> constants;
-            if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
-                query = "SELECT * FROM Links;";
-            else if (restrictions[0] != constants.Any && restrictions[1] == constants.Any && restrictions[2] == constants.Any)
-                query = "SELECT * FROM Links WHERE id = " + std::to_string(restrictions[0]) + ";";
-            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] == constants.Any)
-                query = "SELECT * FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + ";";
-            else if (restrictions[0] == constants.Any && restrictions[1] == constants.Any && restrictions[2] != constants.Any)
-                query = "SELECT * FROM Links WHERE to_id = " + std::to_string(restrictions[2]) + ";";
-            else if (restrictions[0] == constants.Any && restrictions[1] != constants.Any && restrictions[2] != constants.Any)
-                query = "SELECT * FROM Links WHERE from_id = " + std::to_string(restrictions[1]) + "AND to_id = "
-                        + std::to_string(restrictions[2]) + ";";
-            auto result = line.retrieve(line.insert(query));
-            std::vector<Link<TLink>> links {};
-            Link<TLink> link {};
-            for(int i{}; i<result.size(); i++)
-            {
-                for(int j{}; j<3; j++)
-                {
-                    link[j] = result[i][j].as<TLink>();
-                    links.push_back(link);
-                }
-            }
-            return links;
-        }
+    private: void GetIndex()
+    {
+        auto r = line.retrieve(line.insert("SELECT * FROM Links;"));
+        index = r.size();
+    }
 
-        private: std::string opts{};
-        private: std::string query{};
-        private: std::uint64_t _index{};
-        
-        private: void GetIndex()
-        {
-            auto r = line.retrieve(line.insert("SELECT * FROM Links;"));
-            _index = r.size();
-        }
-        
-        private: pqxx::connection connection {opts};
-        private: pqxx::work transaction {connection};
-        private: pqxx::pipeline line {transaction};
-    };
-}
+    private: pqxx::connection connection {opts};
+    private: pqxx::work transaction {connection};
+    private: pqxx::pipeline line {transaction};
+};
--- cpp/PSQLBench.cpp
+++ cpp/PSQLBench.cpp
@@ -1,22 +1,21 @@
-namespace Platform::Data::Doublets::Benchmarks
+#include "LinksPSQL.h"
+#include <benchmark/benchmark.h>
+
+const std::string OPTS = "host=localhost user=mitron57 dbname=mitron57 password='mitron57' port=5432";
+LinksPSQL<int> table(OPTS);
+
+static void BM_CreateMillionLinks(benchmark::State& state)
 {
-    using namespace Platform::Data::Doublets;
-
-    const std::string OPTS = "";
-    LinksPSQL<int> table(OPTS);
-
-    static void BM_CreateMillionLinks(benchmark::State& state)
+    for(auto _ : state)
     {
-        for(auto _ : state)
+        for(std::size_t i = 0; i<state.range(0); ++i)
         {
-            for(std::size_t i = 0; i<state.range(0); ++i)
-            {
-                std::vector sub{i+1, i+1};
-                table.Create(sub);
-            }
-            table.Complete();
+            std::vector sub{i+1, i+1};
+            table.Create(sub);
         }
+        table.Complete();
     }
-
-    BENCHMARK(BM_CreateMillionLinks)->Arg(1000000);
 }
+
+BENCHMARK(BM_CreateMillionLinks)->Arg(1000000);
+BENCHMARK_MAIN();
