378bf5a95dc2c99d29277a545422cac8f4568f3f
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2022-08-30 22:25:00 +0400

    Convert Client to named fields

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -4,17 +4,20 @@ use async_trait::async_trait;
 use postgres::Error;
 use tokio_postgres as postgres;
 
-pub struct Client(u64, postgres::Client);
+pub struct Client {
+    index: u64,
+    client: postgres::Client,
+}
 
 impl Client {
     pub async fn new(client: postgres::Client) -> Result<Client, Error> {
         let index = client.query("SELECT * FROM Links;", &[]).await?.len() as u64;
-        Ok(Self(index, client))
+        Ok(Self { index, client })
     }
 
     pub async fn transaction(&mut self) -> Result<Transaction<'_>, Error> {
-        let transaction = self.1.transaction().await.unwrap();
-        Ok(Transaction::new(transaction, self.0))
+        let transaction = self.client.transaction().await.unwrap();
+        Ok(Transaction::new(transaction, self.index))
     }
 }
 
@@ -22,11 +25,11 @@ impl Client {
 impl Cruds<'_, postgres::Client> for Client {
     #[inline]
     fn index(&mut self) -> &mut u64 {
-        &mut self.0
+        &mut self.index
     }
 
     #[inline]
     fn executor(&mut self) -> &postgres::Client {
-        &self.1
+        &self.client
     }
 }
